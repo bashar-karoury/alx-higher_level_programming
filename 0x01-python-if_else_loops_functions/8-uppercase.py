00e3a1566bb8698d0cc842c3edb4348bbecf643c
bashar-karoury <basharalkaroury@hotmail.com>
2023-11-28 19:43:40 +0200

    Add 8-upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    for c in str:
+        if ord(c) > 96:
+            print("{0:c}".format(ord(c) - 0x20), end='')
+        else:
+            print(c, end='')
+    print()
