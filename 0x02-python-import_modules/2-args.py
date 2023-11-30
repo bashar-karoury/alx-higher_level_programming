8e3dba1a1d423481d0adf4b5dda4723f8d020ea4
bashar-karoury <basharalkaroury@hotmail.com>
2023-11-30 11:52:18 +0200

    Add

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+argv_len = len(sys.argv)
+if __name__ == "__main__":
+    if argv_len == 1:
+        print("{} arguments.".format(argv_len))
+    else:
+        print("{} arguments:".format(argv_len))
+        for i in range(1, argv_len):
+            print("{}: {}".format(i, sys.argv[i]))
