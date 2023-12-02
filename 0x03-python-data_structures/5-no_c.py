751cafea247c3653f10bc9750aabebd9f351b04b
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-02 21:55:58 +0200

    Add_no_c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    new_string = ""
+    for a in my_string:
+        if a == 'c' or a == 'C':
+            continue
+        else:
+            new_string = new_string + a
+    return new_string
