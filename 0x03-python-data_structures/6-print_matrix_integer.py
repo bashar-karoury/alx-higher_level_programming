5d839c2ea83fe99c1a3590fb5b3d75b6cc8f4d5f
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-02 22:15:28 +0200

    Fix 6-print_matix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,6 +7,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             if first is False:
                 print(" ", end='')
-            print("{}".format(j), end='')
+            print("{:d}".format(j), end='')
             first = False
         print("")
