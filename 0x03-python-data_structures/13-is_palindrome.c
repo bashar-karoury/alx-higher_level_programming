8dc327255a5028a8ec26377f9bcab2b7c077b989
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-04 18:01:45 +0200

    Try not allocating

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: double pointer to head node
@@ -17,12 +16,12 @@ int is_palindrome(listint_t **head)
 		return (1);
 
 	listint_t *trav_node = *head;
-	listint_t *reverse = NULL;
+/*	listint_t *reverse = NULL; */
 
 	/* traverse to last node and copy in reverse order*/
 	while (trav_node != NULL)
 	{
-		add_nodeint_begin(&reverse, trav_node->n);
+		/* add_nodeint_begin(&reverse, trav_node->n); */
 		trav_node = trav_node->next;
 	}
 
@@ -30,17 +29,17 @@ int is_palindrome(listint_t **head)
 	/* now compare *head with reverse */
 	while (trav_node != NULL)
 	{
-		if (trav_node->n != reverse->n)
+/*		if (trav_node->n != reverse->n)
 		{
 			result = 0;
 			break;
 		}
-
+*/
 		trav_node = trav_node->next;
-		reverse = reverse->next;
+/*		reverse = reverse->next; */
 	}
 
-	free_listint(reverse);
+	/* free_listint(reverse); */
 	return (result);
 }
 /**
