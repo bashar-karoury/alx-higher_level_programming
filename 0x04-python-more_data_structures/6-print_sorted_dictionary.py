e8e0aa7c4064e40aeb9a890b306f352743270c05
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-07 00:46:45 +0200

    Fix print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     sorted_keys = list(a_dictionary)
     sorted_keys.sort()
     for k in sorted_keys:
-        print("{0}: {1}".format(k, a_dictionary[k]))    
+        print("{0}: {1}".format(k, a_dictionary[k]))
