7be42d7053a82e8ffe00d7abe7fa4f7802722c0f
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-07 02:24:58 +0200

    Add roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    current_rank = 0
+    number = 0
+    for c in roman_string:
+        if c in roman:
+            rank = roman[c]
+            if rank > current_rank:
+                number = rank - number
+            else:
+                number = rank + number
+            current_rank = rank
+        else:
+            return None
+    return number
