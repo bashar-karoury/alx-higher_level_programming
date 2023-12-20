3084e31bbb621df19b596aab7ca03ee5911a3e3c
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-21 01:29:46 +0200

    Add 103-magic_class

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import math
+"""Module that provides MagicClass
+
+Classes:
+    MagicClass: defines a circle
+"""
+
+
+class MagicClass:
+    """Magic class
+    Attributes:
+        radius (int): radius of circle
+    """
+    def __init__(self, radius):
+        """Initializes a new instance of the class.
+
+        Args:
+        radius (int): radius of circle
+        """
+        if type(radius) != int and type(radius) != float:
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        """ Calculate area of circle
+        Return (int): result
+        """
+        return math.pi * (self.radius ** 2)
+
+    def circumference(self):
+        """ Calculate circumference of circle
+
+        Returns(int): result
+        """
+        return (math.pi * 2 * self.radius)
