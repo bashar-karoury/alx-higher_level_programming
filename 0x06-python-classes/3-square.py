4e95b155b39240e0740491f595078892a1ed04c9
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-20 13:15:55 +0200

    Add 3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module that provides Square
+
+Classes:
+    Square: simple square class
+"""
+
+
+class Square:
+    """Square class
+    Attributes:
+        __size (int): size of square must be >= 0
+        ...
+    """
+    def __init__(self, size=0):
+        """Initializes a new instance of the class.
+
+        Args:
+        size (float): size of square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Get area of square instance
+        Returns:
+            int: area of square
+        """
+        return ((self.__size)**2)
