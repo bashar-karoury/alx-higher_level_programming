735af0b4d4cab37964a6969effbc6b80b9e824a0
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-12 11:28:58 +0200

    Add task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class Testing_max_integer(unittest.TestCase):
+    """ Unit testing class subbed from unittest.TestCase
+    """
+
+    def test_empty_list(self):
+        """ testing function takes empty list
+        """
+        self.assertEqual(max_integer([]), None)
+
+    def test_One_element_list(self):
+        """ testing functin takes list with one argument
+        """
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_Neg_element_list(self):
+        """ testing functin takes list with one neg argument
+        """
+        self.assertEqual(max_integer([-1]), -1)
+
+    def test_Normal_list(self):
+        """ testing functin takes normal list
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_Reverse_list(self):
+        """ testing functin takes reveresed ordered list
+        """
+        self.assertEqual(max_integer([14, 3, 4]), 14)
