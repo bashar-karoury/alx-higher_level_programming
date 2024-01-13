788eb78cb60c08473c66d792313ea65bb4aa43da
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-13 18:01:14 +0200

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module that contains Square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square Class
+    """
+
+    def __init__(self, size):
+        """ Init function
+
+            Args:
+                size (int): size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ function to return area of square
+
+            Returns:
+                int: area of square
+        """
+        return self.__size ** 2
