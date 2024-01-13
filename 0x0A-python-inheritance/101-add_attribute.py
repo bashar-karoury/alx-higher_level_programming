4d4cc23c1fb48da3a573400c7ec7af0ceb169813
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-13 21:23:57 +0200

    task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that contains add_attribute
+"""
+
+
+def add_attribute(obj, name, value):
+    """ function that adds new attribute to object if it is
+        possible
+
+        Args:
+            obj (object): any object
+            name : name of attribute
+            value: value of attribute to be added
+    """
+    if type(obj).__module__ == 'builtins' or hasattr(obj, name):
+        raise TypeError("can't add new attribute ")
+    else:
+        obj.name = value
