3bbef172d4cea59624de8652d42254877d204a74
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-13 15:52:07 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module contains incomplete implementation
+"""
+
+
+class BaseGeometry:
+    """ Class that contains un finished function
+    """
+
+    def area(self):
+        """ unfinished implementation
+        """
+        raise Exception("area() is not implemented")
