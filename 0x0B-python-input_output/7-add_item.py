248466aefd5414deb68fd0c06cdb2fb1b46ba98d
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-15 12:39:55 +0200

    fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Module that continas scirpt to add args to a python list
-    and then save them to a file
-"""
+    and then save them to a file """
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
