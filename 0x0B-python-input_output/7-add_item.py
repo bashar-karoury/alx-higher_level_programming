bb6ff77c1a662f58f1044029b19cdcecc8e7ed82
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-15 12:46:52 +0200

    fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Module load add save """
+
+
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 filename = "add_item.json"
 list_a = load_from_json_file(filename)
 args = sys.argv[1:]
