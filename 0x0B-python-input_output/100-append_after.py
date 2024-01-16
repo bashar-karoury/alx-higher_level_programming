2036bd7678deb725864d8048c6023817bd8f08ab
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-16 14:27:55 +0200

    fix task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,6 +4,14 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """ function that append a line of string after each line
+        containing specific line string
+
+        Args:
+            filename (str): filename to read from and edit
+            search_string(str): string to search for in lines
+            new_string(str): string line to append after found lines
+    """
     with open(filename, 'r+', encoding="utf-8") as f:
         old_txt = ""
         for line in f:
