482a404b0704d170e9d0cde8f142293fafdc19a2
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-16 10:44:19 +0200

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Module of Student Class
+"""
+
+
+class Student:
+    """ Student Class
+    """
+    def __init__(self, first_name, last_name, age):
+        """ init function
+
+            Args:
+                first_name (str): first name
+                last_name (str): second name
+                age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ function that retrieve a dictionary rep of a Student instance
+            Args:
+                attrs (list): filter of strings
+            Returns:
+                (dict): dictonray rep
+        """
+        dic = self.__dict__
+        if attrs is None:
+            return dic
+        else:
+            return {attr: dic[attr] for attr in attrs if attr in dic}
+
+    def reload_from_json(self, json):
+        """ function that replaces all attributes of the instance
+
+            Args:
+                json (dict): dictionary
+        """
+        for k in json:
+            setattr(self, k, json[k])
