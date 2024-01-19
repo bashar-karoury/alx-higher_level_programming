0fb3c004b6380d778ebfbe7af96b11e8ddba9149
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-19 18:33:31 +0400

    task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,3 +70,11 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set
+        """
+        obj = cls(1, 1, 0, 0, 100)
+        obj.update(**dictionary)
+        return obj
