2d2c30d3930eeaa6d684c7645c6bf48c34023843
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-19 14:01:37 +0400

    Edit display 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -121,7 +121,11 @@ class Rectangle(Base):
     def display(self):
         """ display rec in stdout
         """
+        for y in range(self.y):
+            print("")
         for i in range(self.height):
+            for x in range(self.x):
+                print(' ', end="")
             for j in range(self.width):
                 print('#', end='')
             print("")
