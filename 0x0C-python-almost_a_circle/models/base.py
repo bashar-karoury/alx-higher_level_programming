a44170d4ed958b00a2304d9e693404ce6e15785d
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-19 08:31:34 +0400

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module of Base class
+"""
+
+
+class Base:
+    """ Base class for all other classes
+        to manage id attribute of sub classes
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ init method for objects of this class
+
+           Args:
+                id (int) : id of object
+        """
+        if id is None:
+            __nb_objects += 1
+            self.id = __nb_objects
+        else:
+            self.id = id
