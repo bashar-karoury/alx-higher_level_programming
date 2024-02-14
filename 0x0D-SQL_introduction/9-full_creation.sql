f58890b10dad277c9f86d74aa75ea9fa22a0e65e
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-14 21:42:16 +0400

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- create tables and insert records
+-- CREATE table in database
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+--add records
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+--add records
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+--add records
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+--add records
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
