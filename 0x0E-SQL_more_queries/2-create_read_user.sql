4037b2d2bba0edeefa6f13ca08a158284dc43a27
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-15 17:09:50 +0400

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creates database and  user in data base with all privileges
+-- create user with its privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT PRIVILEGES ON *.* TO user_0d_2@localhost
+
