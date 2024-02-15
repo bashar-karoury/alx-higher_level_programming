4ad77f04999769c510bb9cbb2ef90c0680451496
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-15 22:39:41 +0400

    Remove USE statements

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- Lists all citys of California that can be found in database
 -- from tables cities and states
-USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE state_id = (SELECT states.id FROM states WHERE states.name = 'California') ORDER BY cities.id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,3 @@
 -- Lists all citys of California that can be found in database
 -- from tables cities and states
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name FROM cities JOIN states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
