d85a4af2686941b8057f13c5131cd7ea43870e1a
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-15 22:35:52 +0400

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,4 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre
 -- linkded sorted by show title and genre id
-SELECT title, tv_genres.id as genre_id FROM tv_shows JOIN tv_show_genres JOIN tv_genres ON tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+USE hbtn_0d_tvshows;
+SELECT title, tv_genres.id AS genre_id FROM tv_shows JOIN tv_show_genres JOIN tv_genres ON tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
