9a440f580a3fee92c831cb5c51a37b5ed2a77726
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-15 18:47:11 +0400

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates database and  user in data base with all privileges
+-- create user with its privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE states (
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+);
