d2c9e3628b4ef42ca777116971a2ea222e2a6e81
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-15 15:53:40 +0400

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates user in data base with all privileges
 -- create user with its privileges
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1_pwd@localhost
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost
 
