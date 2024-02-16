1f02411e1a16ecbe06c17be1c944e2a1211411df
bashar-karoury <basharalkaroury@hotmail.com>
2024-02-16 11:23:52 +0400

    task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,3 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre
 -- linkded sorted by show title and genre id
-SELECT tv_shows.title, tv_genres.id AS genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON
- tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, tv_show_genre
-s.genre_id ASC;
+SELECT tv_shows.title, tv_genres.id AS genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
