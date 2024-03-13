5c0f47761a4119496974c2749e397f8131979e05
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-13 22:26:35 +0400

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str = str + c;
+      }
+      console.log(str);
+    }
+  }
+}
+module.exports = Square;
