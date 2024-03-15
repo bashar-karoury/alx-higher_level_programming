ef3db39ad3c51088c803e6ba239dbbabd61df21b
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-15 11:41:39 +0400

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """ Script to list all states from database hbtn_0e_0_usa
 """
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-db = MySQLdb.connect(
-                    host="localhost",
-                    user=username,
-                    passwd=password,
-                    db=database
-                )
-cur = db.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM states JOIN cities ON states.id = cities.state_id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=username,
+                        passwd=password,
+                        db=database
+                    )
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM states"
+                " JOIN cities ON states.id = cities.state_id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
