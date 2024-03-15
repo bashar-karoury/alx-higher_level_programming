3eb9d1630bed87f77aa983499f8cdc1d1e907ab2
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-15 11:36:10 +0400

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 """ Script to list all states from database hbtn_0e_0_usa
 """
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-if ';' not in state_name:
-    db = MySQLdb.connect(
-                    host="localhost",
-                    user=username,
-                    passwd=password,
-                    db=database
-                )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'"
-                "ORDER BY id".format(state_name))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if ';' not in state_name:
+        db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=username,
+                        passwd=password,
+                        db=database
+                    )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name='{}'"
+                    "ORDER BY id".format(state_name))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-    cur.close()
-    db.close()
+        cur.close()
+        db.close()
