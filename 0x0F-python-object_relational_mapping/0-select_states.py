0a60b53a8c60c14dd333cab993a9a554192434d6
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-15 11:07:18 +0400

    fix task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ password = sys.argv[2]
 database = sys.argv[3]
 
 db = MySQLdb.connect(
-                    host="localhost:3306",
+                    host="localhost",
                     port="3306",
                     user=username,
                     passwd=password,
