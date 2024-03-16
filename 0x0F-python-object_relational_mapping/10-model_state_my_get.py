d39c0de613efdf2be27886274ca08f654a060fa9
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-16 14:05:41 +0400

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""list all state objects from database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            mysql_username,
+                            mysql_passwd,
+                            mysql_db_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_1 = session.query(State).filter_by(name=state_name).one()
+    print(state_1.id)
