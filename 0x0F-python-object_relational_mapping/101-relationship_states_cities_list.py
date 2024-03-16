1a5e962caf7a6cc046c4ed1394a6fb491acf1041
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-17 00:04:46 +0400

    task 101

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""list all state objects from database
+"""
+import sys
+from relationship_state import State
+from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            mysql_username,
+                            mysql_passwd,
+                            mysql_db_name))
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+    california = State(name='California')
+    all_states = session.query(State).order_by(State.id.asc()).all()
+    for state in all_states:
+        print("{}: {}".format(state.id, state.name))
+        state_cities = session.query(City).order_by(
+                            City.id.asc()).filter_by(state=state)
+        for city in state_cities:
+            print("\t{}: {}".format(city.id, city.name))
