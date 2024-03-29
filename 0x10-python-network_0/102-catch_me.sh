ef3c7dfcfa7cf6d86a02555cc79a3fe1ac1abf53
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-29 07:07:15 +0400

    task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script to display response of GET request
-curl -s -X PUT -d "user_id=98" -L 0.0.0.0:5000/catch_me_3
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School
+"
