9152054e81ea2f7ff3ccd0ccf3410862b89795fa
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-29 14:58:29 +0400

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This Module takes url as an argument, and fetch X-Request-Id value
+"""
+import requests
+
+
+import sys
+try:
+    r = requests.get(sys.argv[1])
+    value = r.headers["X-Request-Id"]
+    print("{}".format(value))
+except Exception as e:
+    print(e)
