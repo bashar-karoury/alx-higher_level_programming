503ef4010f79e9138cd2156e88346c50957f748b
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-29 14:00:35 +0400

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,10 +3,10 @@
 """
 import urllib.request
 import sys
-req = urllib.request.Request(sys.argv[1])
 try:
+    req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         value = response.headers["X-Request-Id"]
         print("{}".format(value))
-except urllib.error.URLError as e:
-    print(e)
+except e:
+    pass
