35b5aaedc3f677f1b4e90d2349cb6403fbcd4de2
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-29 04:53:08 +0400

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,10 +15,10 @@ def find_peak(l):
     middle = int(len(l)/2)
 
     # check middle if a find_peak
-    if l[middle] <= l[middle - 1]:
+    if middle > 0 and l[middle] <= l[middle - 1]:
         # go left
         return find_peak(l[:middle])
-    elif l[middle] <= l[middle + 1]:
+    elif middle < len(l) - 1 and l[middle] <= l[middle + 1]:
         # go right
         return find_peak(l[middle+1:])
     else:
