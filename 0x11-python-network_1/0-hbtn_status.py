050c00dd52fe319f228fd79378d75b056a04b89a
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-29 13:44:13 +0400

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,11 @@
 import urllib.request
 
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print("\t- type: {}".format(type(the_page)))
-    print("\t- content: {}".format(the_page))
-    print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
+try:
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
+except:
+    pass
