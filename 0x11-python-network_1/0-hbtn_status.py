34586493e4f87a9cf46e31a7a7ca84aaad77889a
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-29 13:39:22 +0400

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This script fetch a url and displays response body
+"""
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    print("\t- type: {}".format(type(the_page)))
+    print("\t- content: {}".format(the_page))
+    print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
