5f1d91e8a593fcd56a859c18d7af16c4d76494fa
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-30 14:09:47 +0400

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,18 +7,16 @@ try:
     url = " https://api.github.com/repos/{}/{}/commits?per_page=10".format(
         sys.argv[1], sys.argv[2])
     headers = {"Accept": "application/vnd.github.v3+json"}
-    print(url)
     r = requests.get(url, headers=headers)
     if r.status_code < 400:
         results_json = r.json()
         if results_json:
             counter = 0
-            for commit in results_json:
-                if counter >= 10:
-                    break
-                author = commit['commit']['author']
-                print("{}: {}".format(commit['sha'], author['name']))
-                counter += 1
+            commits = results_json
+            for i in range(10):
+                print("{}: {}".format(
+                    commits[i].get("sha"),
+                    commits[i].get("commit").get("author").get("name")))
         else:
             print("No result")
     else:
