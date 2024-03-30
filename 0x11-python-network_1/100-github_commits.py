f02ab3a90aa5aef193a03f99889820f16adfe65d
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-30 13:58:29 +0400

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,7 +4,7 @@
 import requests
 import sys
 try:
-    url = " https://api.github.com/repos/{}/{}/commits".format(
+    url = " https://api.github.com/repos/{}/{}/commits?per_page=10".format(
         sys.argv[1], sys.argv[2])
     headers = {"Accept": "application/vnd.github.v3+json"}
     r = requests.get(url, headers=headers)
