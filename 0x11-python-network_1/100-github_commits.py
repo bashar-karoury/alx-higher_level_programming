709b3f782493f6843245e0af87c19feaeb73360f
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-30 13:49:11 +0400

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This Module takes url as an argument, and fetch X-Request-Id value
+"""
+import requests
+import sys
+try:
+    url = " https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1], sys.argv[2])
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    r = requests.get(url, headers=headers)
+    if r.status_code < 400:
+        results_json = r.json()
+        if results_json:
+            counter = 0
+            for commit in results_json:
+                if counter > 10:
+                    break
+                author = commit['commit']['author']
+                print("{}: {}".format(commit['sha'], author['name']))
+                counter += 1
+        else:
+            print("No result")
+    else:
+        print("None")
+except Exception as e:
+    print(e)
+    print("Not a valid JSON")
