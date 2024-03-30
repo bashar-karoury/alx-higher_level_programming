48f22d3e9089be5afc7881b7571ee774f2c5aa3a
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-30 12:18:39 +0400

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,20 +5,21 @@ import requests
 import sys
 try:
     data = {}
-    if len(sys.argv) >= 3:
-        data = {"q": sys.argv[2]}
+    if len(sys.argv) >= 2:
+        data = {"q": sys.argv[1]}
     else:
         data = {"q": ""}
-    r = requests.post(sys.argv[1], data)
+    r = requests.post("http://0.0.0.0:5000/search_user", data)
 
     if r.status_code < 400:
         results_json = r.json()
         if results_json:
-            print("[{}] {}".format(results_json[id], results_json[name]))
+            print("[{}] {}".format(results_json['id'], results_json['name']))
         else:
             print("No result")
 # except requests.exceptions.JSONDecodeError as e:
 #    print("Not a valid JSON")
 
 except Exception as e:
+    print(e)
     print("Not a valid JSON")
