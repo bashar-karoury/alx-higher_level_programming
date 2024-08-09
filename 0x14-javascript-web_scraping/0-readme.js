652186a8a9d21fa3950f51b9afee87b308d2ad30
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-09 23:22:29 +0300

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const path = process.argv[2];
 
 fs.readFile(path, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
