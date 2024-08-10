98702b8f107ea6f563d374259c82f520824b3e06
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-10 14:26:36 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,12 @@
 const request = require('request');
 const url = process.argv[2];
 const id = 18;
-const character = `http://swapi.co/api/people/${id}/`;
 request(url, function (error, response, body) {
   if (!error) {
     const data = JSON.parse(body);
     let count = 0;
-
+    const rootUrl = `${response.request.uri.protocol}//${response.request.uri.host}`;
+    const character = `${rootUrl}/api/people/${id}/`;
     for (const result of data.results) {
       if (result.characters.includes(character)) {
         count++;
