69407133d2b7c4fa41649131c8f1aeb4bfc45073
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-10 13:46:16 +0300

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-console.log(url);
 request(url, function (error, response, body) {
   if (!error) {
     const data = JSON.parse(body);
