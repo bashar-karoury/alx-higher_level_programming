2ccd2a31b0c392903864746bf941b87f38fe82e7
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-10 13:53:54 +0300

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+// const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/people/18';
+request(url, function (error, response, body) {
+  if (!error) {
+    const data = JSON.parse(body);
+    console.log(data.films.length);
+  }
+});
